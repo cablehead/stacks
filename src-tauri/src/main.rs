ca3616a8ca7ce0b81b7bf1079d35b65873849f37
Andy Gayton <andy@thecablelounge.com>
2023-04-25 15:04:52 -0400

    add shortcut handler to hide / show UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{GlobalShortcutManager, Manager};
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let main_window = app.get_window("main").unwrap();
+
+            let mut shortcut = app.global_shortcut_manager();
+            shortcut
+                .register("Cmd+Shift+G", move || {
+                    if main_window.is_visible().unwrap() {
+                        main_window.hide().unwrap();
+                    } else {
+                        main_window.show().unwrap();
+                        main_window.set_focus().unwrap();
+                    }
+                })
+                .unwrap_or_else(|err| println!("{:?}", err));
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
