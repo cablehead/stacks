4bd2bdecfef5dae2d934d707208696c8fbedd559
Andy Gayton <andy@thecablelounge.com>
2023-04-26 10:05:17 -0400

    start to make options data that's pushed from Rust

--- app.jsx
+++ app.jsx
@@ -1,18 +1,23 @@
 import { render } from "preact";
+import { signal } from "@preact/signals";
+
+const options = signal([
+  "Option 1",
+  "Option 2",
+  "Option 3",
+  "Option 4",
+]);
 
 function App() {
   return (
     <main>
-      <div style={{paddingBottom: "0.5rem", borderBottom: "solid 1px #333"}}>
+      <div style={{ paddingBottom: "0.5rem", borderBottom: "solid 1px #333" }}>
         <div>
           <input type="text" placeholder="Type a command..." />
         </div>
       </div>
       <div class="results">
-        <div>Command 1</div>
-        <div>Command 2</div>
-        <div>Command 3</div>
-        <div>Command 4</div>
+        {options.value.map((option) => <div>{option}</div>)}
       </div>
     </main>
   );
