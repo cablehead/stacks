a4fc4b8df02d85fbdaba9846966208683391cf57
Andy Gayton <andy@thecablelounge.com>
2023-05-02 17:02:27 +0200

    rename receiver to consumer

--- src-tauri/src/producer.rs
+++ src-tauri/src/producer.rs
@@ -14,11 +14,11 @@ impl Producer {
     }
 
     fn add_consumer(&self) -> (Vec<String>, flume::Receiver<String>) {
-        let receiver = {
-            let (sender, receiver) = flume::unbounded();
+        let consumer = {
+            let (sender, consumer) = flume::unbounded();
             let mut senders = self.senders.lock().unwrap();
             senders.push(sender);
-            receiver
+            consumer
         };
 
         let data = {
@@ -26,7 +26,7 @@ impl Producer {
             data.clone()
         };
 
-        (data, receiver)
+        (data, consumer)
     }
 
     fn consumer_count(&self) -> usize {
@@ -37,9 +37,7 @@ impl Producer {
     fn send_data(&self, item: String) {
         self.data.lock().unwrap().push(item.clone());
 
-        // Get a copy of the current senders
         let mut senders = self.senders.lock().unwrap();
-
         // Remove disconnected senders and send data to connected ones
         senders.retain(|sender| {
             if sender.is_disconnected() {
@@ -70,21 +68,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_data_sent_to_consumer() {
+    fn test_producer() {
         let producer = Arc::new(Producer::new());
 
-        let (initial_data, receiver) = producer.add_consumer();
+        let (initial_data, consumer1) = producer.add_consumer();
         // Check that the initial data is empty (since the producer hasn't sent anything yet)
         assert!(initial_data.is_empty());
 
         // Data to send
-        let (sender, iterator_receiver) = flume::unbounded();
-
+        let (sender, recver) = flume::unbounded();
         // Start a new thread for the producer to run the command
         let producer_thread = {
             let producer = producer.clone();
             std::thread::spawn(move || {
-                producer.run(iterator_receiver.into_iter());
+                producer.run(recver.into_iter());
             })
         };
 
@@ -92,16 +89,16 @@ mod tests {
         sender.send("Hello, World!".to_string()).unwrap();
         sender.send("Another string".to_string()).unwrap();
 
-        // Check that the receiver receives the correct data
+        // Check that the consumer receives the correct data
         for _ in 0..2 {
-            match receiver.recv() {
+            match consumer1.recv() {
                 Ok(data) => assert!(["Hello, World!", "Another string"].contains(&data.as_str())),
                 Err(_) => panic!("Failed to receive data"),
             }
         }
 
         // Add another consumer
-        let (initial_data, receiver2) = producer.add_consumer();
+        let (initial_data, consumer2) = producer.add_consumer();
 
         // Check that the initial data contains the first two data
         assert_eq!(
@@ -113,14 +110,14 @@ mod tests {
         sender.send("And another one".to_string()).unwrap();
         sender.send("Final string".to_string()).unwrap();
 
-        // Check that both receivers receive the correct data
+        // Check that both consumers receive the correct data
         for _ in 0..2 {
-            match receiver.recv() {
+            match consumer1.recv() {
                 Ok(data) => assert!(["And another one", "Final string"].contains(&data.as_str())),
                 Err(_) => panic!("Failed to receive data"),
             }
 
-            match receiver2.recv() {
+            match consumer2.recv() {
                 Ok(data) => assert!(["And another one", "Final string"].contains(&data.as_str())),
                 Err(_) => panic!("Failed to receive data"),
             }
@@ -128,16 +125,16 @@ mod tests {
 
         assert_eq!(producer.consumer_count(), 2);
 
-        // Drop receiver1 (simulate consumer leaving)
-        drop(receiver);
+        // Drop consumer1 (simulate consumer leaving)
+        drop(consumer1);
 
         // Send two more items
         sender.send("Extra item 1".to_string()).unwrap();
         sender.send("Extra item 2".to_string()).unwrap();
 
-        // Check that receiver2 receives the correct data
+        // Check that consumer2 receives the correct data
         for _ in 0..2 {
-            match receiver2.recv() {
+            match consumer2.recv() {
                 Ok(data) => assert!(["Extra item 1", "Extra item 2"].contains(&data.as_str())),
                 Err(_) => panic!("Failed to receive data"),
             }
@@ -148,7 +145,7 @@ mod tests {
         drop(sender);
         producer_thread.join().unwrap();
 
-        match receiver2.recv() {
+        match consumer2.recv() {
             Err(_) => {}
             _ => panic!("Expected RecvError"),
         }
