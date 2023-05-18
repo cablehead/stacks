6791401e17afeba443da4ca7bfc390b3e88d5461
Andy Gayton <andy@thecablelounge.com>
2023-05-18 10:19:17 -0400

    go direct to xs for new clipboard items

--- src-tauri/src/clipboard.rs
+++ src-tauri/src/clipboard.rs
@@ -1,10 +1,11 @@
-use std::io::Write;
 use std::path::PathBuf;
 
 use tauri::api::process::{Command, CommandEvent};
 
+use crate::xs;
+
 pub fn start(path: &PathBuf) {
-    let path = path.clone().into_os_string().into_string().unwrap();
+    let path = path.clone();
     let (mut rx, _child) = Command::new_sidecar("x-macos-pasteboard")
         .unwrap()
         .spawn()
@@ -12,20 +13,10 @@ pub fn start(path: &PathBuf) {
 
     tauri::async_runtime::spawn(async move {
         while let Some(event) = rx.recv().await {
-            let path = path.clone();
             if let CommandEvent::Stdout(line) = event {
-                let mut child = std::process::Command::new("xs")
-                    .arg(path)
-                    .arg("put")
-                    .arg("--topic")
-                    .arg("clipboard")
-                    .stdin(std::process::Stdio::piped())
-                    .spawn()
-                    .unwrap();
-                if let Some(ref mut stdin) = child.stdin {
-                    stdin.write_all(line.as_bytes()).unwrap();
-                }
-                let _ = child.wait();
+                let path = path.clone();
+                let env = xs::store_open(&path);
+                log::info!("{}", xs::store_put(&env, Some("clipboard".into()), None, line));
             }
         }
     });
