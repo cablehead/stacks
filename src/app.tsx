56a9d9beaf5b2ed278ccb6e13dffbddd6fa81d10
Andy Gayton <andy@thecablelounge.com>
2023-06-20 12:52:12 -0400

    refactor: move the LeftPane nav out to its own file

--- src/app.tsx
+++ src/app.tsx
@@ -5,9 +5,9 @@ import { invoke } from "@tauri-apps/api/tauri";
 import { writeText } from "@tauri-apps/api/clipboard";
 import { hide } from "tauri-plugin-spotlight-api";
 
-import { Icon } from "./ui/icons";
-import { borderRight, darkThemeClass, lightThemeClass } from "./ui/app.css";
+import { darkThemeClass, lightThemeClass } from "./ui/app.css";
 
+import { Nav } from "./panels/nav";
 import { StatusBar } from "./panels/statusbar";
 import { MetaPanel } from "./panels/meta";
 import { Actions, attemptAction } from "./panels/actions";
@@ -28,77 +28,6 @@ import {
   updateSelected,
 } from "./state";
 
-function LeftPane() {
-  const RowIcon = ({ item }: { item: Item }) => {
-    switch (item.content_type) {
-      case "Image":
-        return <Icon name="IconImage" />;
-
-      case "Link":
-        return <Icon name="IconLink" />;
-
-      case "Text":
-        return <Icon name="IconClipboard" />;
-    }
-
-    return <Icon name="IconBell" />;
-  };
-
-  const TerseRow = ({ item, index }: { item: Item; index: number }) => (
-    <div
-      className={"terserow" +
-        (index === stack.selected.value ? " selected" : "")}
-      onClick={() => stack.selected.value = index}
-      style="
-        display: flex;
-        width: 100%;
-        gap: 0.5ch;
-        overflow: hidden;
-        padding: 0.5ch 0.75ch;
-        border-radius: 6px;
-        cursor: pointer;
-        "
-    >
-      <div
-        style={{
-          flexShrink: 0,
-          width: "2ch",
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-        }}
-      >
-        <RowIcon item={item} />
-      </div>
-
-      <div
-        style={{
-          flexGrow: 1,
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-        }}
-      >
-        {item.terse}
-      </div>
-    </div>
-  );
-  return (
-    <div
-      className={borderRight}
-      style="
-      flex: 1;
-      max-width: 20ch;
-      overflow-y: auto;
-      padding-right: 0.5rem;
-    "
-    >
-      {stack.items.value
-        .map((item, index) => {
-          return <TerseRow item={item} index={index} />;
-        })}
-    </div>
-  );
-}
 
 function RightPane(
   { item, content }: {
@@ -261,7 +190,7 @@ function Main() {
             position: relative;
         ">
         <div style="display: flex; height: 100%; overflow: hidden; gap: 0.5ch;">
-          <LeftPane />
+          <Nav />
           <RightPane
             item={selectedItem.value}
             content={selectedContent.value}
---
+++ src/panels/nav.tsx
@@ -0,0 +1,77 @@
+import { Icon } from "../ui/icons";
+import { borderRight } from "../ui/app.css";
+
+import { Item, stack } from "../state";
+
+export function Nav() {
+  return (
+    <div
+      className={borderRight}
+      style="
+      flex: 1;
+      max-width: 20ch;
+      overflow-y: auto;
+      padding-right: 0.5rem;
+    "
+    >
+      {stack.items.value
+        .map((item, index) => {
+          return <TerseRow item={item} index={index} />;
+        })}
+    </div>
+  );
+}
+
+const RowIcon = ({ item }: { item: Item }) => {
+  switch (item.content_type) {
+    case "Image":
+      return <Icon name="IconImage" />;
+
+    case "Link":
+      return <Icon name="IconLink" />;
+
+    case "Text":
+      return <Icon name="IconClipboard" />;
+  }
+
+  return <Icon name="IconBell" />;
+};
+
+const TerseRow = ({ item, index }: { item: Item; index: number }) => (
+  <div
+    className={"terserow" +
+      (index === stack.selected.value ? " selected" : "")}
+    onClick={() => stack.selected.value = index}
+    style="
+        display: flex;
+        width: 100%;
+        gap: 0.5ch;
+        overflow: hidden;
+        padding: 0.5ch 0.75ch;
+        border-radius: 6px;
+        cursor: pointer;
+        "
+  >
+    <div
+      style={{
+        flexShrink: 0,
+        width: "2ch",
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+      }}
+    >
+      <RowIcon item={item} />
+    </div>
+
+    <div
+      style={{
+        flexGrow: 1,
+        whiteSpace: "nowrap",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+      }}
+    >
+      {item.terse}
+    </div>
+  </div>
+);
