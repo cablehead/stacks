9fca5290bf9f2d7767b147afa6c7ed0485bab6aa
Andy Gayton <andy@thecablelounge.com>
2023-06-29 19:03:34 -0400

    some clean up

--- src/stacks.tsx
+++ src/stacks.tsx
@@ -1,18 +1,14 @@
 import { computed, effect, Signal, signal } from "@preact/signals";
-
 import { hide } from "tauri-plugin-spotlight-api";
 import { listen } from "@tauri-apps/api/event";
 import { writeText } from "@tauri-apps/api/clipboard";
 import { invoke } from "@tauri-apps/api/tauri";
-
-import { Item, LoadedItem, Stack } from "./types";
-
+import { Item, LoadedItem } from "./types";
 import { filterContentTypeMode, mainMode } from "./modals";
 
-// TODO: cap size of CAS, with MRU eviction
 const CAS: Map<string, string> = new Map();
 
-export async function getContent(hash: string): Promise<string> {
+async function getContent(hash: string): Promise<string> {
   const cachedItem = CAS.get(hash);
   if (cachedItem !== undefined) {
     return cachedItem;
@@ -23,68 +19,42 @@ export async function getContent(hash: string): Promise<string> {
   return content;
 }
 
-export const createStack = (items: Signal<Item[]>, parent?: Stack): Stack => {
-  const selected = signal(0);
+const items = signal<Item[]>([]);
+const selected = signal(0);
+const loadedHash: Signal<string> = signal("");
+const loadedContent: Signal<string> = signal("");
 
-  const loadedHash: Signal<string> = signal("");
-  const loadedContent: Signal<string> = signal("");
+const normalizedSelected = computed(() => {
+  let val = selected.value % (items.value.length);
+  if (val < 0) val = items.value.length + val;
+  return val;
+});
 
-  const selectedContent = computed((): string | undefined => {
-    const item = items.value[normalizedSelected.value];
-    if (item === undefined) return undefined;
-    if (item.hash !== loadedHash.value) return undefined;
-    return loadedContent.value;
-  });
+const selectedContent = computed((): string | undefined => {
+  const item = items.value[normalizedSelected.value];
+  if (item === undefined || item.hash !== loadedHash.value) return undefined;
+  return loadedContent.value;
+});
 
-  const loaded = computed((): LoadedItem | undefined => {
-    const item = items.value[normalizedSelected.value];
-    if (item === undefined) return undefined;
-    const content = selectedContent.value;
-    if (content === undefined) return undefined;
-    return {
-      item,
-      content,
-    };
-  });
+const loaded = computed((): LoadedItem | undefined => {
+  const item = items.value[normalizedSelected.value];
+  const content = selectedContent.value;
+  if (item === undefined || content === undefined) return undefined;
+  return { item, content };
+});
 
-  const normalizedSelected = computed(() => {
-    let val = selected.value % (items.value.length);
-    if (val < 0) val = items.value.length + val;
-    return val;
-  });
+async function updateLoaded(hash: string) {
+  loadedContent.value = await getContent(hash);
+  loadedHash.value = hash;
+}
 
-  async function updateLoaded(hash: string) {
-    loadedContent.value = await getContent(hash);
-    loadedHash.value = hash;
+effect(() => {
+  const item = items.value[normalizedSelected.value];
+  if (item && item.hash != loadedHash.value) {
+  console.log("EFFECT", "updateLoaded", "proceed");
+    updateLoaded(item.hash);
   }
-
-  effect(() => {
-  console.log("EFFECT", "updateLoaded");
-    const item = items.value[normalizedSelected.value];
-    if (item === undefined) return undefined;
-    if (item.hash != loadedHash.value) {
-      updateLoaded(item.hash);
-    }
-  });
-
-  console.log("createStack", parent);
-  // const parents = parent ? [parent, ...parent.parents] : [];
-
-  /*
-  effect(() => {
-    console.log("SLECECTED CHANGED", parents.length > 0 ? parents[0].selected.value : -6000, selected.value);
-  });
-  */
-
-  return {
-    items,
-    selected,
-    normalizedSelected,
-    loaded,
-  };
-};
-
-export const items = signal<Item[]>([]);
+});
 
 const updateItems = async (filter: string, contentType: string) => {
   items.value = await invoke<Item[]>("store_list_items", {
@@ -95,23 +65,21 @@ const updateItems = async (filter: string, contentType: string) => {
 
 const d1 = await listen("refresh-items", () => {
   console.log("Data pushed from Rust");
-  updateItems(
-    mainMode.state.curr.value,
-    filterContentTypeMode.curr.value,
-  );
+  updateItems(mainMode.state.curr.value, filterContentTypeMode.curr.value);
 });
 console.log("init my listen", d1);
 
 effect(() => {
   console.log("EFFECT", "updateItems");
-  updateItems(
-    mainMode.state.curr.value,
-    filterContentTypeMode.curr.value,
-  );
+  updateItems(mainMode.state.curr.value, filterContentTypeMode.curr.value);
 });
 
-const root = createStack(items);
-export const currStack = root;
+export const currStack = {
+  items,
+  selected,
+  normalizedSelected,
+  loaded,
+};
 
 export async function triggerCopy() {
   const loaded = currStack.loaded.value;
