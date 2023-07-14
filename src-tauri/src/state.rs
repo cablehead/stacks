c88d63ee3127887d90a0f98d0a1fda14c7c0a3c8
Andy Gayton <andy@thecablelounge.com>
2023-07-14 14:26:42 -0400

    wip

--- src-tauri/src/state.rs
+++ src-tauri/src/state.rs
@@ -9,10 +9,15 @@ pub struct State {
 }
 
 impl State {
-    pub fn new(db_path: &str) -> Self {
+        pub fn new(db_path: &str) -> Self {
+        let store = Store::new(db_path);
+        let mut stack = Stack::new();
+        for frame in store.list() {
+            stack.create_or_merge(&store, &frame);
+        }
         Self {
-            stack: Stack::new(),
-            store: Store::new(db_path),
+            stack,
+            store,
         }
     }
 }
