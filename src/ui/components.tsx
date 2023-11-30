abbe9bcc7cb3b1b4ca4c902ab0e14fabc9a5fd05
Andy Gayton <andy@thecablelounge.com>
2023-11-30 00:29:40 -0500

    pull VertDiv out to a common module

---
+++ src/ui/components.tsx
@@ -0,0 +1,11 @@
+import { borderRight } from "./app.css";
+
+export const VertDiv = () => (
+  <div
+    className={borderRight}
+    style={{
+      width: "1px",
+      height: "1.5em",
+    }}
+  />
+);
