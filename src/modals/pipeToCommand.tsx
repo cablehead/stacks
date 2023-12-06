5134cdc8d2774257a060616e5aba92f3e664c72c
Andy Gayton <andy@thecablelounge.com>
2023-12-06 15:52:10 -0500

    hide filter panel, and add shim

--- src/modals/pipeToCommand.tsx
+++ src/modals/pipeToCommand.tsx
@@ -77,97 +77,105 @@ export default {
 
     return (
       <div
-        className={overlay}
         style={{
-          position: "absolute",
-          overflow: "auto",
-          fontSize: "0.9rem",
-          bottom: "2ch",
-          right: "2ch",
-          left: "2ch",
-          top: "2ch",
-          borderRadius: "0.5rem",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "transparent",
           zIndex: 1000,
+          position: "absolute",
+          padding: "1ch",
         }}
       >
         <div
+          className={overlay}
           style={{
-            display: "flex",
-            flexDirection: "column",
-            height: "100%",
-            justifyContent: "space-between",
+            fontSize: "0.9rem",
+            padding: "2ch",
+            borderRadius: "0.5rem",
+            width: "calc(100% - 2ch)",
+            height: "calc(100% - 2ch)",
           }}
         >
           <div
             style={{
-              height: "4lh",
-              minHeight: "4lh",
-              overflow: "auto",
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              justifyContent: "space-between",
             }}
           >
-            <textarea
-              ref={inputRef}
-              spellcheck={false}
+            <div
               style={{
-                width: "100%",
-                margin: "2ch",
-                outline: "none",
-                border: "none",
-              }}
-              placeholder="Shell command ..."
-              onInput={(event) => {
-                state.curr.value = (event.target as HTMLTextAreaElement).value;
-              }}
-              onKeyDown={(event) => {
-                event.stopPropagation();
-                switch (true) {
-                  case event.key === "Escape":
-                    event.preventDefault();
-                    modes.deactivate();
-                    break;
-
-                  case event.metaKey && event.key === "Enter":
-                    state.accept_meta(stack, modes);
-                    break;
-                }
+                height: "6lh",
+                minHeight: "6lh",
+                overflow: "auto",
               }}
             >
-            </textarea>
-          </div>
-
-          <div
-            style={{
-              whiteSpace: "pre",
-              overflow: "auto",
-              width: "100%",
-              flex: "1",
-              padding: "1ch 3ch",
-              boxShadow: `0 -2px 5px ${vars.shadowColor}`,
-              backgroundColor: vars.backgroundColor,
-              color: vars.textColor,
-              borderColor: vars.borderColor,
-            }}
-          >
-            {state.res.value.out}
-          </div>
-          {state.res.value.err != "" &&
-            (
-              <div
+              <textarea
+                ref={inputRef}
+                spellcheck={false}
                 style={{
-                  whiteSpace: "pre",
                   width: "100%",
-                  flex: "1",
-                  overflow: "auto",
-                  padding: "1ch 3ch",
-                  boxShadow: `0 -2px 5px ${vars.shadowColor}`,
-                  backgroundColor: vars.backgroundColor,
-                  color: vars.textColor,
-                  borderColor: vars.borderColor,
+                  margin: "2ch",
+                  outline: "none",
+                  border: "none",
+                }}
+                placeholder="Shell command ..."
+                onInput={(event) => {
+                  state.curr.value =
+                    (event.target as HTMLTextAreaElement).value;
+                }}
+                onKeyDown={(event) => {
+                  event.stopPropagation();
+                  switch (true) {
+                    case event.key === "Escape":
+                      event.preventDefault();
+                      modes.deactivate();
+                      break;
+
+                    case event.metaKey && event.key === "Enter":
+                      state.accept_meta(stack, modes);
+                      break;
+                  }
                 }}
               >
-                {state.res.value.err}
-              </div>
-            )}
+              </textarea>
+            </div>
+
+            <div
+              style={{
+                whiteSpace: "pre",
+                overflow: "auto",
+                width: "100%",
+                flex: "1",
+                padding: "1ch 3ch",
+                boxShadow: `0 -2px 3px ${vars.shadowColor}`,
+                backgroundColor: vars.backgroundColor,
+                color: vars.textColor,
+                borderColor: vars.borderColor,
+              }}
+            >
+              {state.res.value.out}
+            </div>
+            {state.res.value.err != "" &&
+              (
+                <div
+                  style={{
+                    whiteSpace: "pre",
+                    width: "100%",
+                    flex: "1",
+                    overflow: "auto",
+                    padding: "1ch 3ch",
+                    boxShadow: `0 -2px 3px ${vars.shadowColor}`,
+                    backgroundColor: vars.backgroundColor,
+                    color: vars.textColor,
+                    borderColor: vars.borderColor,
+                  }}
+                >
+                  {state.res.value.err}
+                </div>
+              )}
+          </div>
         </div>
       </div>
     );
