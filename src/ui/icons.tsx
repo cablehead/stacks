a4964456ea2251bf8b48fe8b6edad0aa8a050706
Andy Gayton <andy@thecablelounge.com>
2024-01-08 20:51:02 -0500

    style: tweak shift and command key icons

--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -392,7 +392,7 @@ const IconCommandKey: FunctionComponent = () => (
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
     height="1em"
-    viewBox="0 0 24 24"
+    viewBox="0 1 24 24"
     fill="none"
     stroke="currentColor"
     stroke-width="1.5"
@@ -415,7 +415,7 @@ const IconShiftKey: FunctionComponent = () => (
     stroke-width="1.5"
     fill="currentColor"
     class="bi bi-shift"
-    viewBox="0 0 16 16"
+    viewBox="0 2 16 16"
   >
     <path d="M7.27 2.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H11.5v3a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-3H1.654C.78 10.5.326 9.455.924 8.816L7.27 2.047zM14.346 9.5 8 2.731 1.654 9.5H4.5a1 1 0 0 1 1 1v3h5v-3a1 1 0 0 1 1-1z" />
   </svg>
