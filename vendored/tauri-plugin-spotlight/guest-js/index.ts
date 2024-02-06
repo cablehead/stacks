1e0a54f7097d3c2bc95f989464d7d5736ed2622b
Andy Gayton <andy@thecablelounge.com>
2024-02-05 22:39:29 -0500

    remove vendored tauri-plugin-spotlight

--- vendored/tauri-plugin-spotlight/guest-js/index.ts
+++
@@ -1,9 +0,0 @@
-import { invoke } from '@tauri-apps/api/tauri'
-
-export async function show () {
-  void invoke('plugin:spotlight|show')
-}
-
-export async function hide () {
-  void invoke('plugin:spotlight|hide')
-}
