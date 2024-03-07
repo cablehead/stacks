0b4673f5bed830c9f09814a950e421ddd9e63b66
Andy Gayton <andy@thecablelounge.com>
2024-03-07 13:09:59 -0500

    wip

--- src-tauri/src/http.rs
+++ src-tauri/src/http.rs
@@ -1,13 +1,16 @@
-use std::convert::Infallible;
 use std::error::Error;
+use std::str::FromStr;
 
 use tokio::net::UnixListener;
 
-use hyper::service::service_fn;
-use hyper::{Request, Response};
+use http_body_util::{Empty, Full};
 use hyper::body::Bytes;
-use http_body_util::Full;
 use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Method, StatusCode};
+// use hyper::body::Frame;
+use http_body_util::{combinators::BoxBody, BodyExt};
+use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 
 // use tracing::error;
@@ -153,8 +156,40 @@ async fn post(
 async fn handle(
     _state: SharedState,
     _app_handle: tauri::AppHandle,
-    _req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    let path = req.uri().path();
+    let id = path
+        .strip_prefix("/")
+        .and_then(|id| scru128::Scru128Id::from_str(id).ok());
+
+    match (req.method(), id) {
+        (&Method::GET, Some(id)) =>
+        // get(id, state).await,
+        {
+            Ok(Response::new(full(id.to_string())))
+        }
+        // (&Method::POST, None) if path == "/" => post(req, state.clone(), app_handle.clone()).await,
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+// https://hyper.rs/guides/1/server/echo/
+// We create some utility functions to make Empty and Full bodies
+// fit our broadened Response body type.
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
 
 pub fn start(app_handle: tauri::AppHandle, state: SharedState) {
@@ -172,7 +207,12 @@ pub fn start(app_handle: tauri::AppHandle, state: SharedState) {
 
             tauri::async_runtime::spawn(async move {
                 if let Err(err) = http1::Builder::new()
-                    .serve_connection(io, service_fn(move |req| handle(state_cloned.clone(), app_handle_clonded.clone(), req)))
+                    .serve_connection(
+                        io,
+                        service_fn(move |req| {
+                            handle(state_cloned.clone(), app_handle_clonded.clone(), req)
+                        }),
+                    )
                     .await
                 {
                     // Match against the error kind to selectively ignore `NotConnected` errors
