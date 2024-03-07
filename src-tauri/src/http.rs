fa7e803a46fe381d1117115c94330eaef77e92b5
Andy Gayton <andy@thecablelounge.com>
2024-03-07 15:04:06 -0500

    wip

--- src-tauri/src/http.rs
+++ src-tauri/src/http.rs
@@ -1,15 +1,17 @@
 use std::error::Error;
 use std::str::FromStr;
 
+use futures_util::TryStreamExt;
+
 use tokio::net::UnixListener;
 
+use http_body_util::{combinators::BoxBody, BodyExt, StreamBody};
 use http_body_util::{Empty, Full};
 use hyper::body::Bytes;
+use hyper::body::Frame;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Method, StatusCode};
-// use hyper::body::Frame;
-use http_body_util::{combinators::BoxBody, BodyExt};
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 
@@ -17,6 +19,7 @@ use hyper_util::rt::TokioIo;
 
 use crate::state::SharedState;
 // use crate::store::{infer_mime_type, InProgressStream, MimeType};
+use crate::store::MimeType;
 // use crate::ui::generate_preview;
 
 /*
@@ -153,40 +156,81 @@ async fn post(
 }
 */
 
+type BoxError = Box<dyn std::error::Error + Send + Sync>;
+type HTTPResult = Result<Response<BoxBody<Bytes, BoxError>>, BoxError>;
+
 async fn handle(
-    _state: SharedState,
+    state: SharedState,
     _app_handle: tauri::AppHandle,
     req: Request<hyper::body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+) -> HTTPResult {
     let path = req.uri().path();
     let id = path
         .strip_prefix("/")
         .and_then(|id| scru128::Scru128Id::from_str(id).ok());
 
     match (req.method(), id) {
-        (&Method::GET, Some(id)) =>
-        // get(id, state).await,
-        {
-            Ok(Response::new(full(id.to_string())))
-        }
+        (&Method::GET, Some(id)) => get(id, state).await,
         // (&Method::POST, None) if path == "/" => post(req, state.clone(), app_handle.clone()).await,
-        _ => {
-            let mut not_found = Response::new(empty());
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
+        _ => response_404(),
+    }
+}
+
+fn response_404() -> HTTPResult {
+    let mut not_found = Response::new(empty());
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok(not_found)
+}
+
+async fn get(id: scru128::Scru128Id, state: SharedState) -> HTTPResult {
+    let (item, meta) = state.with_lock(|state| {
+        let item = state.view.items.get(&id).cloned();
+        let meta = item
+            .as_ref()
+            .and_then(|i| state.store.get_content_meta(&i.hash));
+        (item, meta)
+    });
+
+    match item {
+        Some(item) => {
+            let cache_path = state.with_lock(|state| state.store.cache_path.clone());
+            let reader = cacache::Reader::open_hash(cache_path, item.hash)
+                .await
+                .unwrap();
+
+            let stream = tokio_util::io::ReaderStream::new(reader);
+            let stream = stream
+                .map_ok(Frame::data)
+                .map_err(|e| Box::new(e) as BoxError); // Convert to BoxError
+            let body = BodyExt::boxed(StreamBody::new(stream));
+
+            let content_type = match meta {
+                Some(meta) => match meta.mime_type {
+                    MimeType::TextPlain => "text/plain",
+                    MimeType::ImagePng => "image/png",
+                },
+                None => "application/octet-stream",
+            };
+
+
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", content_type)
+                .body(body)?)
         }
+        None => response_404(),
     }
 }
 
 // https://hyper.rs/guides/1/server/echo/
 // We create some utility functions to make Empty and Full bodies
 // fit our broadened Response body type.
-fn empty() -> BoxBody<Bytes, hyper::Error> {
+fn empty() -> BoxBody<Bytes, BoxError> {
     Empty::<Bytes>::new()
         .map_err(|never| match never {})
         .boxed()
 }
-fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, BoxError> {
     Full::new(chunk.into())
         .map_err(|never| match never {})
         .boxed()
