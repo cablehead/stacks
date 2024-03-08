0700f3e5c011151c710aa72e8fe77bab1bcbf880
Andy Gayton <andy@thecablelounge.com>
2024-03-07 20:15:49 -0500

    feat: the HTTP interface now listens on a UNIX domain socket (#52)
    chore: update hyper from 0.14.27 to 1.2

--- src-tauri/src/http.rs
+++ src-tauri/src/http.rs
@@ -1,21 +1,33 @@
-use futures::StreamExt;
+use std::error::Error;
 use std::str::FromStr;
+
+use futures_util::TryStreamExt;
+
+use tokio::net::UnixListener;
+
 use tauri::Manager;
 
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
-
-use tracing::error;
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full, StreamBody};
+use hyper::body::Bytes;
+use hyper::body::Frame;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Method, StatusCode};
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
 
 use crate::state::SharedState;
 use crate::store::{infer_mime_type, InProgressStream, MimeType};
 use crate::ui::generate_preview;
 
+type BoxError = Box<dyn std::error::Error + Send + Sync>;
+type HTTPResult = Result<Response<BoxBody<Bytes, BoxError>>, BoxError>;
+
 async fn handle(
-    req: Request<Body>,
     state: SharedState,
     app_handle: tauri::AppHandle,
-) -> Result<Response<Body>, Error> {
+    req: Request<hyper::body::Incoming>,
+) -> HTTPResult {
     let path = req.uri().path();
     let id = path
         .strip_prefix("/")
@@ -23,15 +35,12 @@ async fn handle(
 
     match (req.method(), id) {
         (&Method::GET, Some(id)) => get(id, state).await,
-        (&Method::POST, None) if path == "/" => post(req, state.clone(), app_handle.clone()).await,
-        _ => Ok(Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::from("Not Found"))
-            .unwrap()),
+        (&Method::POST, None) if path == "/" => post(req, state, app_handle).await,
+        _ => response_404(),
     }
 }
 
-async fn get(id: scru128::Scru128Id, state: SharedState) -> Result<Response<Body>, Error> {
+async fn get(id: scru128::Scru128Id, state: SharedState) -> HTTPResult {
     let (item, meta) = state.with_lock(|state| {
         let item = state.view.items.get(&id).cloned();
         let meta = item
@@ -46,7 +55,12 @@ async fn get(id: scru128::Scru128Id, state: SharedState) -> Result<Response<Body
             let reader = cacache::Reader::open_hash(cache_path, item.hash)
                 .await
                 .unwrap();
-            let stream = Body::wrap_stream(tokio_util::io::ReaderStream::new(reader));
+
+            let stream = tokio_util::io::ReaderStream::new(reader);
+            let stream = stream
+                .map_ok(Frame::data)
+                .map_err(|e| Box::new(e) as BoxError);
+            let body = BodyExt::boxed(StreamBody::new(stream));
 
             let content_type = match meta {
                 Some(meta) => match meta.mime_type {
@@ -59,21 +73,28 @@ async fn get(id: scru128::Scru128Id, state: SharedState) -> Result<Response<Body
             Ok(Response::builder()
                 .status(StatusCode::OK)
                 .header("Content-Type", content_type)
-                .body(stream)
-                .unwrap())
+                .body(body)?)
         }
-        None => Ok(Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::from("Not Found"))
-            .unwrap()),
+        None => response_404(),
     }
 }
 
+#[derive(serde::Deserialize, serde::Serialize, Debug, Clone, PartialEq)]
+pub struct Content {
+    pub mime_type: MimeType,
+    pub content_type: String,
+    pub terse: String,
+    pub tiktokens: usize,
+    pub words: usize,
+    pub chars: usize,
+    pub preview: String,
+}
+
 async fn post(
-    req: Request<Body>,
+    req: Request<hyper::body::Incoming>,
     state: SharedState,
     app_handle: tauri::AppHandle,
-) -> Result<Response<Body>, Error> {
+) -> HTTPResult {
     let mut streamer = state.with_lock(|state| {
         let stack = state.get_curr_stack();
         state.ui.select(None); // focus first
@@ -84,50 +105,33 @@ async fn post(
         streamer
     });
 
-    let mut bytes_stream = req.into_body();
+    let mut body = req.into_body();
 
-    #[derive(serde::Deserialize, serde::Serialize, Debug, Clone, PartialEq)]
-    pub struct Content {
-        pub mime_type: MimeType,
-        pub content_type: String,
-        pub terse: String,
-        pub tiktokens: usize,
-        pub words: usize,
-        pub chars: usize,
-        pub preview: String,
-    }
+    while let Some(frame) = body.frame().await {
+        let data = frame?.into_data().unwrap();
+        streamer.append(&data);
+        let preview = generate_preview(
+            "dark",
+            &Some(streamer.content.clone()),
+            &MimeType::TextPlain,
+            &"Text".to_string(),
+            true,
+        );
 
-    while let Some(chunk) = bytes_stream.next().await {
-        match chunk {
-            Ok(chunk) => {
-                streamer.append(&chunk);
-                let preview = generate_preview(
-                    "dark",
-                    &Some(streamer.content.clone()),
-                    &MimeType::TextPlain,
-                    &"Text".to_string(),
-                    true,
-                );
+        let content = String::from_utf8_lossy(&streamer.content);
+        let content = Content {
+            mime_type: MimeType::TextPlain,
+            content_type: "Text".to_string(),
+            terse: content.chars().take(100).collect(),
+            tiktokens: 0,
+            words: content.split_whitespace().count(),
+            chars: content.chars().count(),
+            preview,
+        };
 
-                let content = String::from_utf8_lossy(&streamer.content);
-                let content = Content {
-                    mime_type: MimeType::TextPlain,
-                    content_type: "Text".to_string(),
-                    terse: content.chars().take(100).collect(),
-                    tiktokens: 0,
-                    words: content.split_whitespace().count(),
-                    chars: content.chars().count(),
-                    preview,
-                };
-
-                app_handle
-                    .emit_all("streaming", (streamer.packet.id, content))
-                    .unwrap();
-            }
-            Err(e) => {
-                tracing::error!("Error reading bytes from HTTP POST: {}", e);
-            }
-        }
+        app_handle
+            .emit_all("streaming", (streamer.packet.id, content))
+            .unwrap();
     }
 
     state.with_lock(|state| {
@@ -137,30 +141,68 @@ async fn post(
     });
     app_handle.emit_all("refresh-items", true).unwrap();
 
+    let response_body = streamer.packet.id.to_string();
     Ok(Response::builder()
         .status(StatusCode::OK)
-        .body(Body::from(streamer.packet.id.to_string()))
-        .unwrap())
+        .body(full(response_body))?)
 }
 
-pub fn start(app_handle: tauri::AppHandle, state: SharedState) {
+fn response_404() -> HTTPResult {
+    Ok(Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(empty())?)
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, BoxError> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn empty() -> BoxBody<Bytes, BoxError> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+pub fn start(app_handle: tauri::AppHandle, state: SharedState, db_path: &str) {
+    let socket_path = std::path::Path::new(db_path).join("sock");
+    let _ = std::fs::remove_file(&socket_path);
+    let listener = UnixListener::bind(socket_path).unwrap();
+
     tauri::async_runtime::spawn(async move {
-        let addr = ([127, 0, 0, 1], 9146).into();
+        loop {
+            let (stream, _) = listener.accept().await.unwrap();
+            let io = TokioIo::new(stream);
 
-        let make_svc = make_service_fn(move |_conn| {
-            let state = state.clone();
-            let app_handle = app_handle.clone();
-            async move {
-                Ok::<_, hyper::Error>(service_fn(move |req: Request<Body>| {
-                    handle(req, state.clone(), app_handle.clone())
-                }))
-            }
-        });
+            let state_cloned = state.clone();
+            let app_handle_clonded = app_handle.clone();
 
-        let server = Server::bind(&addr).serve(make_svc);
-
-        if let Err(e) = server.await {
-            error!("server error: {}", e);
+            tauri::async_runtime::spawn(async move {
+                if let Err(err) = http1::Builder::new()
+                    .serve_connection(
+                        io,
+                        service_fn(move |req| {
+                            handle(state_cloned.clone(), app_handle_clonded.clone(), req)
+                        }),
+                    )
+                    .await
+                {
+                    // Match against the error kind to selectively ignore `NotConnected` errors
+                    if let Some(std::io::ErrorKind::NotConnected) =
+                        err.source().and_then(|source| {
+                            source
+                                .downcast_ref::<std::io::Error>()
+                                .map(|io_err| io_err.kind())
+                        })
+                    {
+                        // Silently ignore the NotConnected error
+                    } else {
+                        // Handle or log other errors
+                        println!("Error serving connection: {:?}", err);
+                    }
+                }
+            });
         }
     });
 }
