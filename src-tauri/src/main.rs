6fe23b864b338dfdb30b68b481cd98b546209ed6
Andy Gayton <andy@thecablelounge.com>
2025-08-15 01:18:12 -0400

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![recursion_limit = "512"]
+#![allow(unexpected_cfgs)]
 
 use tracing::info;
 
